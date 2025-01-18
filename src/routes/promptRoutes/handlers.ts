3dad3051c65c8369c297e8690d4379a7fd168eef
ankit_gupta <ankit.gupta@kapture.com>
2025-01-18 05:56:30 +0530

    added awareness

--- src/routes/promptRoutes/handlers.ts
+++ src/routes/promptRoutes/handlers.ts
@@ -1,21 +1,40 @@
 import { Request, Response } from "express";
 import chatGpt from "../../OpenAi";
+import type { ChatCompletionMessageParam } from "openai/resources";
 import pdf from "pdf-parse";
 import db from "@/drizzle";
 import Chats from "@/models/Chats";
 export const handleUserPropmts = async (req: Request, res: Response) => {
   try {
-    const { prompt, threadId }: { prompt: string; threadId?: number } =
+    const { prompt, threadId }: { prompt: string; threadId?: string } =
       req.body;
     const { id } = req.decoded;
+    console.log(typeof threadId, "req.body");
 
+    let messages: Array<ChatCompletionMessageParam> = [{ role: "user", content: prompt }];
+
+    if (threadId) {
+      let prevPrompt = {...messages[0]}
+      prevPrompt.role = "assistant"
+      const previousMessages = await db.query.Chats.findMany({
+        where: (chats, { eq, and }) =>
+          and(eq(chats.threadId, parseInt(threadId))),
+        orderBy: (chats, { asc }) => [asc(chats.createdAt)],
+      });
+      console.log(previousMessages, "previousMessages");
+      messages = previousMessages
+        .flatMap((msg) => [
+          { role:msg.userMessage? "user":"system", content: (msg.userMessage ?? msg.gptResponse )|| ""  },
+        ])
+        messages.push(prevPrompt)            
+    }
     const resp = await chatGpt.chat.completions.create({
       model: "gpt-4o",
-      messages: [{ role: "user", content: prompt }],
+      messages,
     });
     if (threadId) {
       await db.insert(Chats).values({
-        threadId,
+        threadId:parseInt(threadId),
         userMessage: prompt,
         userId: id,
         gptResponse: resp.choices[0].message.content,
