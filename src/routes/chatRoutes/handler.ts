d31a3ce6aa94ba07b5708bf7dcc0c18709afc9dd
Ankit Gupta <aryangupta2224@gmail.com>
2025-01-19 16:30:01 +0530

    added delete thread api

--- src/routes/chatRoutes/handler.ts
+++ src/routes/chatRoutes/handler.ts
@@ -63,3 +63,16 @@ export const getThreadMessages = async (req: Request, res: Response) => {
 };
 
 
+
+
+export const deleteThread = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.body;
+    await db.delete(Chats).where(eq(Chats.threadId, id));
+    await db.delete(MessageThread).where(eq(MessageThread.id, id));
+    return res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- src/routes/chatRoutes/index.ts
+++ src/routes/chatRoutes/index.ts
@@ -1,6 +1,6 @@
 import { checkToken } from "@/utils"
 import express from "express"
-import { createThread, getThreadMessages, getThreads } from "./handler"
+import { createThread, deleteThread, getThreadMessages, getThreads } from "./handler"
 
 
 
@@ -9,6 +9,7 @@ const router = express.Router()
 router.get("/get-threads",checkToken,getThreads)
 router.post("/create-thread",checkToken,createThread)
 router.get("/get-thread-messages/:id",checkToken,getThreadMessages)
+router.post("/delete-thread",checkToken,deleteThread)
 
 
 export default router
